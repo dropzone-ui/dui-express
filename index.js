a178303fd41148ee9c0065fc52883b04f1771602
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2022-01-25 10:26:41 -0500

    [INIT]

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const _ = require("lodash");
+const fs = require("fs");
+const app = express();
+
+// enable files upload
+app.use(fileUpload({ createParentPath: true }));
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+//start app
+const port = process.env.PORT || 2800;
+
+app.listen(port, () => console.log(`App is listening on port ${port}.`));
+app.get("/", (req, res) => {
+  res.json({ response: "HAAAAAAAAAAAAAAAAAAAAAAAAAA" });
+});
+app.get("/getAll", async (req,res) => {
+  const testFolder = __dirname;
+  let files = [];
+
+  files = await fs.readdirSync(testFolder);
+  res.json(files );
+});
+app.post("/upload-my-file", async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded",
+        payload: {},
+      });
+    } else {
+      //Use the name of the input field (i.e. "file") to retrieve the uploaded file
+      let file = req.files.file;
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      file.mv("./uploads/" + file.name);
+      //send response
+      res.send({
+        status: true,
+        message: "File was uploaded successfully",
+        payload: {
+          name: file.name,
+          mimetype: file.mimetype,
+          size: file.size,
+          path: "/files/uploads/",
+          url: "https://my-ftp-server.com/bjYJGFYgjfVGHVb",
+        },
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      status: false,
+      message: "Unspected problem",
+      payload: {},
+    });
+  }
+});
